27444d66568c5deae70c2e2fa3d9760decfba712
qkshshhd <77177535+qkshshhd@users.noreply.github.com>
2025-07-08 22:27:56 +0900

    Add files via upload
    바보추가

--- Camera.cs
+++ Camera.cs
@@ -22,7 +22,7 @@ public class Camera : MonoBehaviour
         }
         
     }
-
+    //바보
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Wall")
